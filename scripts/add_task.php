2159f459816b89720c2c2e806ccd07ad49f349f0
Vitaliy <gapon007@ukr.net>
2019-02-28 16:52:40 +0200

    Create coonection And put data to DB

---
+++ scripts/add_task.php
@@ -0,0 +1,41 @@
+<?php
+
+
+    $title = trim($_POST['title']);
+    $description = trim($_POST['description']);
+    $checkbox = $_POST['checkboxVisible'];
+
+    //======== Create func Upload img Change name and put in the upload folder ======
+    function UploaddImg($image)
+    {
+        $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+        $filename = uniqid() . "." . $extension;
+        move_uploaded_file($image['tmp_name'], "../uploads/" . $filename);
+        return $filename;
+    }
+
+    $filename = UploaddImg($_FILES['image']);
+
+    //==============ENG:: block get data ================
+
+
+    //====== Connect to DB ======
+    $db = new PDO("mysql:host=webdewel.mysql.tools;dbname=webdewel_task", 'webdewel_task', 'dP_0l2Yx6*');
+
+    //====== Insert data ======
+    $sql = "INSERT INTO tasks (title, description, image, checkbox) VALUE (:title, :description, :image, :checkbox)";
+
+    //====== prepare data ======
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(":title", $title);
+    $stmt->bindParam(":description", $description);
+    $stmt->bindParam(":image", $filename);
+    $stmt->bindParam(":checkbox", $checkbox);
+    $stmt->execute();
+
+
+
+
+
+
+
